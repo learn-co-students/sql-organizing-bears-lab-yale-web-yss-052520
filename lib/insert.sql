52f9d3caa72219916a2eaa325b9c32ce832f7abe
dlee3427 <david.lee.sl2634@yale.edu>
2020-06-03 18:05:00 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES(1, "Mr. Chocolate", 20, "M", "brown", "angry", 2);
+INSERT INTO bears VALUES(2, "Rowdy", 20, "F", "brown", "amicable", 1);
+INSERT INTO bears VALUES(3, "Tabitha", 22, "M", "orange", "curious", 4);
+INSERT INTO bears VALUES(4, "Seargeant Brown", 35, "M", "brown", "angry", 3);
+INSERT INTO bears VALUES(5, "Melissa", 80, "F", "red", "angry", 2);
+INSERT INTO bears VALUES(6, "Grinch", 50, "F", "yellow", "happy", 9);
+INSERT INTO bears VALUES(7, "Wendy", 100, "F", "dark brown", "moderate", 2);
+INSERT INTO bears VALUES(8, null, 90, "M", "brown", "friendly", 1);
