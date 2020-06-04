ab87a385e8e470bbdbfae0c2fa4acd4856b6249d
mlkelly <maurak87@gmail.com>
2020-06-04 09:55:52 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, gender) VALUES ("Mr. Chocolate", "M");
+INSERT INTO bears (name, gender) VALUES ("Rowdy", "M");
+INSERT INTO bears (name, gender) VALUES ("Tabitha", "F");
+INSERT INTO bears (name, gender) VALUES ("Sergeant Brown", "M");
+INSERT INTO bears (name, gender) VALUES ("Melissa", "F");
+INSERT INTO bears (name, gender) VALUES ("Grinch", "M");
+INSERT INTO bears (name, gender) VALUES ("Wendy", "F");
+INSERT INTO bears (name, gender) VALUES (NULL, "M");
+
