aa21d80edaf3e63e1736169db2c6283812c6450a
davidyu7 <david.yu@yale.edu>
2020-06-05 10:26:48 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name, gender) VALUES ("Mr. Chocolate", "M");
+INSERT INTO bears(name, gender) VALUES ("Rowdy", "M");
+INSERT INTO bears(name, gender) VALUES ("Tabitha", "M");
+INSERT INTO bears(name, gender) VALUES ("Seargeant Brown", "M");
+INSERT INTO bears(name, gender) VALUES ("Melissa", "M");
+INSERT INTO bears(name, gender) VALUES ("Grinch", "M");
+INSERT INTO bears(name, gender) VALUES ("Wendy", "M");
+INSERT INTO bears(name, gender) VALUES (NULL, "M");
